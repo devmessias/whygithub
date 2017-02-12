3c3cb9fcb3b0a5fa79d63ed137c95ab36a8bc7ba
bruno messias <devmessias@users.noreply.github.com>
2017-02-12 00:04:27 -0200

    Update and rename whygithub.js to whygithub.user.js

--- whygithub.user.js
+++ whygithub.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         why github?
+// @namespace http://brunomessias.com
 // @include http://github.com/*
 // @include https://github.com/*
-
-// @version      0.1
+// @include https://gist.github.com/*
+// @version      1.0
 // @description  Removes the dark menubar from GitHub.
 // @author       devmessias
 // @grant        none
@@ -12,5 +13,5 @@
 
 (function() {
     'use strict';
-    		document.querySelector('.header-dark').classList.remove('header-dark');
+     document.querySelector('.header-dark').classList.remove('header-dark');
 })();
